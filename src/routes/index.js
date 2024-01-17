791ed8dc5652df148d5b9cf89f888bdaf6745c28
Aum <aumsoni2002@gmail.com>
2024-01-17 14:48:24 -0500

    Update project structure

---
+++ src/routes/index.js
@@ -0,0 +1,34 @@
+// src/routes/index.js
+
+const express = require('express');
+
+// author and version from our package.json file
+// TODO: make sure you have updated your name in the `author` section
+const { version, author } = require('../../package.json');
+
+// Create a router that we can use to mount our API
+const router = express.Router();
+
+/**
+ * Expose all of our API routes on /v1/* to include an API version.
+ */
+router.use(`/v1`, require('./api'));
+
+/**
+ * Define a simple health check route. If the server is running
+ * we'll respond with a 200 OK.  If not, the server isn't healthy.
+ */
+router.get('/', (req, res) => {
+  // Client's shouldn't cache this response (always request it fresh)
+  res.setHeader('Cache-Control', 'no-cache');
+  // Send a 200 'OK' response
+  res.status(200).json({
+    status: 'ok',
+    author,
+    // Use your own GitHub URL for this!
+    githubUrl: 'https://github.com/aumsoni2002/fragments',
+    version,
+  });
+});
+
+module.exports = router;
