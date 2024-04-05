505e5618d4940cb8c29d204f2b1a4967deb1950c
Aum <aumsoni2002@gmail.com>
2024-04-05 11:14:47 -0400

    Adding S3 to our ECS Config v2

--- src/routes/api/delete.js
+++ src/routes/api/delete.js
@@ -1,28 +1,27 @@
 // src/routes/api/delete.js
-
 const logger = require('../../logger');
 const { Fragment } = require('../../model/fragment');
 const { createSuccessResponse, createErrorResponse } = require('../../response');
 
-// DELETE /fragments
+// Deleting a fragment by id
 module.exports.deleteFragment = async (req, res) => {
   const fragmentId = req.params.id;
-  const ownerId = req.user;
+  const userId = req.user;
 
-  logger.info({ fragmentId, ownerId }, `Calling DELETE ${req.originalUrl}`);
+  logger.info({ fragmentId, userId }, `Requesting: DELETE ${req.originalUrl}`);
 
-  // delete the fragment
   try {
-    await Fragment.delete(ownerId, fragmentId);
 
+    await Fragment.delete(userId, fragmentId);
     logger.info(`Fragment was deleted`);
-
     const successResponse = createSuccessResponse({});
     return res.status(200).json(successResponse);
+
   } catch (err) {
+
     const errorResponse = createErrorResponse(404, err.message);
     logger.error({ errorResponse }, 'Failed to delete the fragment');
-
     return res.status(404).json(errorResponse);
+    
   }
 };
